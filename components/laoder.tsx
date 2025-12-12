41549cf56471fb405b2c5855e9a582379f507f27
logicfool <logicfool123121@gmail.com>
2025-12-12 20:24:31 +0530

    fix: image loading

--- components/laoder.tsx
+++ components/laoder.tsx
@@ -49,26 +49,29 @@ const Loader = () => {
         <div className='absolute right-[5%] top-[65%] md:top-[70%] -translate-y-1/2'>
           <MusicToggleButton />
         </div>
-        <div className="relative z-10 w-80 h-80 md:w-96 md:h-96 lg:w-128 lg:h-128">
-          <video
-            className="object-contain"
-            autoPlay
-            loop
-            muted
-            playsInline
-          >
-            {/* Safari Fallback */}
-            {/* <source 
-            src="/assets/videos/loop.mov" 
-            type='video/mp4; codecs="hvc1"' />
-            */}
-            {/* Other Browsers Fallback */}
-            {/* <source 
-            src="/assets/videos/loop.webm" 
-            type="video/webm"></source> */}
-            <source src="/assets/videos/loop.gif" type="image/gif" />
-          </video>
-          <div className='text-center text-kode-monu'>
+        <div className="relative z-10 flex flex-col items-center justify-center">
+          {/* WebM Animation with transparency - only 230KB! */}
+          <div className="w-80 h-80 md:w-96 md:h-96 lg:w-128 lg:h-128 relative">
+            <video
+              className="object-contain w-full h-full"
+              autoPlay
+              loop
+              muted
+              playsInline
+              style={{
+                // Show GIF fallback for Safari which doesn't support WebM alpha
+                backgroundImage: 'url(/assets/videos/loop-small.gif)',
+                backgroundSize: 'contain',
+                backgroundRepeat: 'no-repeat',
+                backgroundPosition: 'center'
+              }}
+            >
+              <source src="/assets/videos/loop-from-gif.webm" type="video/webm" />
+            </video>
+          </div>
+          
+          {/* Loading Text / Enter Button - positioned below GIF */}
+          <div className='text-center text-kode-monu mt-8'>
             {showEnterButton ?
               <Button className='bg-[#DD5622] hover:bg-[#DD5622]/90 text-white ' onClick={enterWorld}>Explore</Button> :
               <span>Loading... {progress}%</span>}
