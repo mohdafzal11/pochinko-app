818e156a914462d44a37bda42bc9e970a4d253ae
logicfool <logicfool123121@gmail.com>
2025-12-12 19:31:19 +0530

    fix: move/modify assets

--- components/laoder.tsx
+++ components/laoder.tsx
@@ -57,20 +57,17 @@ const Loader = () => {
             muted
             playsInline
           >
-            {/* WebM with alpha channel for transparency support (most browsers including mobile) */}
-            {/* <source src="/loop.webm" type="video/webm" /> */}
-            {/* MP4 fallback for browsers that don't support WebM */}
-            {/* <source src="/loop.mp4" type="video/mp4" /> */}
-            <source 
+            {/* Safari Fallback */}
+            {/* <source 
             src="/assets/videos/loop.mov" 
             type='video/mp4; codecs="hvc1"' />
-            <source 
+            */}
+            {/* Other Browsers Fallback */}
+            {/* <source 
             src="/assets/videos/loop.webm" 
-            type="video/webm"></source>
-          </video> 
-              {/* <video width="600" height="100%" autoPlay loop muted playsInline> */}
-              {/* </video> */}
-                
+            type="video/webm"></source> */}
+            <source src="/assets/videos/loop.gif" type="image/gif" />
+          </video>
           <div className='text-center text-kode-monu'>
             {showEnterButton ?
               <Button className='bg-[#DD5622] hover:bg-[#DD5622]/90 text-white ' onClick={enterWorld}>Explore</Button> :
